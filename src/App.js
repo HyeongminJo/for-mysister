6f6ffaf7b6e3897bf1a77b9bdc030d40bcff9828
HyeongminJo <zzz659@naver.com>
2023-10-25 17:53:45 +0900

    메뉴바 router 적용
    231025

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Routes, NavLink, useParams } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="mainDiv">
+      <div className="main1">
+        여기 메인임<br></br>
+        여기 메인임<br></br>
+        여기 메인임<br></br>
+        여기 메인임<br></br>
+        여기 메인임<br></br>
+        여기 메인임<br></br>
+        여기 메인임<br></br>
+      </div>
+      <div className="main2"></div>
+    </div>
+  );
+}
+
+function Inventory(){
+  return (
+    <div>뿌ㅠ쀼</div>
+  );
+}
+
+function About() {
+  return (
+    <div></div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-        <nav className="nav">
-          <div className="navButton">
-            HWARA JO
-          </div>
-          <div className="navButton">
-            INVENTORY
-          </div>
-          <div className="navButton">
-            ABOUT
-          </div>
-          <div className="navButton">
-            *
-          </div>
-        </nav>
-        <div className="main1"></div>
-        <div className="main2"></div>
-        
+      <nav className="nav">
+        <div className="navButton">
+          <NavLink to="/">HWARA JO</NavLink>
+        </div>
+        <div className="navButton">
+          <NavLink to="/Inventory">INVENTORY</NavLink>
+        </div>
+        <div className="navButton">
+          <NavLink to="/About">ABOUT</NavLink>
+        </div>
+        <div className="navButton">
+          <NavLink to="/More">*</NavLink>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Inventory" element={<Inventory />} />
+        <Route path="/*" element={'Not Found'} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Routes, NavLink, useParams } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root')).render(<HashRouter><App /></HashRouter>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

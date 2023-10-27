f207b19d78bcca8e4728dac9dc3844c501a0ef62
HyeongminJo <zzz659@naver.com>
2023-10-27 11:14:04 +0900

    메뉴바 border 설정
    231027

--- src/App.js
+++ src/App.js
@@ -32,7 +32,21 @@ function Home() {
   }, []);
 
     return (
-    <div className="mainDiv">
+    <div className="mainDivHome">
+      <nav className="homeNav">
+        <div className="navButton home">
+          <NavLink to="/">HWARA JO</NavLink>
+        </div>
+        <div className="navButton inventory">
+          <NavLink to="/Inventory">INVENTORY</NavLink>
+        </div>
+        <div className="navButton about">
+          <NavLink to="/About">ABOUT</NavLink>
+        </div>
+        <div className="navButton more">
+          <NavLink to="/More">*</NavLink>
+        </div>
+      </nav>
       <div className="main1" style={{backgroundColor: bgColor}}>
         여기 메인임<br></br>
         여기 메인임<br></br>
@@ -77,29 +91,6 @@ function Inventory(){
 
   return (
     <div className="mainDiv">
-      <div className="main1" style={{backgroundColor: bgColor}}>
-        222222222<br></br>
-        222222222222<br></br>
-        22222222222<br></br>
-        22222222222222<br></br>
-        2222222222222<br></br>
-        2222222222<br></br>
-        2222222222222<br></br>
-      </div>
-      <div className="main2" style={{backgroundColor: bgColor2}}></div>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div></div>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
       <nav className="nav">
         <div className="navButton home">
           <NavLink to="/">HWARA JO</NavLink>
@@ -114,9 +105,214 @@ function App() {
           <NavLink to="/More">*</NavLink>
         </div>
       </nav>
+      <div className="main1" style={{backgroundColor: bgColor}}>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+        222222222<br></br>
+        222222222222<br></br>
+        22222222222<br></br>
+        22222222222222<br></br>
+        2222222222222<br></br>
+        2222222222<br></br>
+        2222222222222<br></br>
+      </div>
+      <div className="main2" style={{backgroundColor: bgColor2}}></div>
+    </div>
+  );
+}
+
+function About() {
+  const colors=[
+    {bgcolor:'#009884'},
+    {bgcolor:'#005528'},
+    {bgcolor:'#6EC8EB'},
+    {bgcolor:'#531916'},
+    {bgcolor:'#FED36F'},
+    {bgcolor:'#B4D2C1'},
+    {bgcolor:'#4C2859'},
+    {bgcolor:'#3981C0'},
+    {bgcolor:'#F19E46'}
+  ];
+
+  const [bgColor, setBgColor] = useState('');
+  const [bgColor2, setBgColor2] = useState('');
+
+  useEffect(() => {
+    const idx = Math.floor(Math.random() * colors.length);
+    setBgColor(colors[idx].bgcolor);
+
+    let idx2;
+    do {
+      idx2 = Math.floor(Math.random() * colors.length);
+    } while (idx2 === idx);
+    
+    setBgColor2(colors[idx2].bgcolor);
+  }, []);
+
+  return (
+    <div className="mainDiv">
+      <nav className="nav">
+        <div className="navButton home">
+          <NavLink to="/">HWARA JO</NavLink>
+        </div>
+        <div className="navButton inventory">
+          <NavLink to="/Inventory">INVENTORY</NavLink>
+        </div>
+        <div className="navButton about">
+          <NavLink to="/About">ABOUT</NavLink>
+        </div>
+        <div className="navButton more">
+          <NavLink to="/More">*</NavLink>
+        </div>
+      </nav>
+      <div className="main1" style={{backgroundColor: bgColor}}>
+        333333<br></br>
+        333333333<br></br>
+        3333333333<br></br>
+        3333333333<br></br>
+        3333333333<br></br>
+        3333333333<br></br>
+        33333333<br></br>
+      </div>
+      <div className="main2" style={{backgroundColor: bgColor2}}></div>
+    </div>
+  );
+}
+
+function More() {
+  const colors=[
+    {bgcolor:'#009884'},
+    {bgcolor:'#005528'},
+    {bgcolor:'#6EC8EB'},
+    {bgcolor:'#531916'},
+    {bgcolor:'#FED36F'},
+    {bgcolor:'#B4D2C1'},
+    {bgcolor:'#4C2859'},
+    {bgcolor:'#3981C0'},
+    {bgcolor:'#F19E46'}
+  ];
+
+  const [bgColor, setBgColor] = useState('');
+  const [bgColor2, setBgColor2] = useState('');
+
+  useEffect(() => {
+    const idx = Math.floor(Math.random() * colors.length);
+    setBgColor(colors[idx].bgcolor);
+
+    let idx2;
+    do {
+      idx2 = Math.floor(Math.random() * colors.length);
+    } while (idx2 === idx);
+    
+    setBgColor2(colors[idx2].bgcolor);
+  }, []);
+
+  return (
+    <div className="mainDiv">
+      <nav className="nav">
+        <div className="navButton home">
+          <NavLink to="/">HWARA JO</NavLink>
+        </div>
+        <div className="navButton inventory">
+          <NavLink to="/Inventory">INVENTORY</NavLink>
+        </div>
+        <div className="navButton about">
+          <NavLink to="/About">ABOUT</NavLink>
+        </div>
+        <div className="navButton more">
+          <NavLink to="/More">*</NavLink>
+        </div>
+      </nav>
+      <div className="main1" style={{backgroundColor: bgColor}}>
+        4444<br></br>
+        4<br></br>
+        4<br></br>
+        4<br></br>
+        444<br></br>
+        444444444444<br></br>
+        444444444444444444444<br></br>
+      </div>
+      <div className="main2" style={{backgroundColor: bgColor2}}></div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Inventory" element={<Inventory />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/More" element={<More />} />
         <Route path="/*" element={'Not Found'} />
       </Routes>
     </div>

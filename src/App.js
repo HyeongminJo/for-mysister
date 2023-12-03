e64f1d1288f123835781fcf750e5e3b87632d48e
HyeongminJo <zzz659@naver.com>
2023-12-03 18:13:30 +0900

    231203
    x

--- src/App.js
+++ src/App.js
@@ -168,7 +168,7 @@ function Inventory(){
                 12222
               </div>
               <div class="item">
-                22222222222aaaa
+                22222222222aaaaa
               </div>
               <div class="item">
                 2222
